6135e824137177cf9f93506b920b26e5e98756b6
ahmed rimon <ahmedrimon377472@gmail.com>
2025-10-15 12:13:10 +0600

    added footer

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,11 +1,13 @@
 import { Outlet } from 'react-router'
 import Navbar from '../pages/Shared/Navbar/Navbar'
+import Footer from '../pages/Shared/Footer/Footer'
 
 export default function RootLayout() {
   return (
     <div>
       <Navbar></Navbar>
         <Outlet></Outlet>
+        <Footer></Footer>
     </div>
   )
 }
